5a69dfa306527fb33da2b7b5709b2e164411269e
alexrandaccio <acrandaccio@gmail.com>
2020-12-08 11:40:58 -0500

    updated HTTP header rules

--- backend/QVtoolapi/QVtoolapi/settings.py
+++ backend/QVtoolapi/QVtoolapi/settings.py
@@ -27,10 +27,13 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = True
 
 # PRODUCTION: only allow api requests from local server
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.radicalxchange.org']
 
 # PRODUCTION: only allow api requests from local server
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "https://radicalxchange.org",
+    "http://localhost:3000",
+    ]
 
 # Application definition
 
