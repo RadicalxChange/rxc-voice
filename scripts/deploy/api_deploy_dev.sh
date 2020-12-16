32c8371460fe99befc0b67ea17a153427ece2695
alexrandaccio <acrandaccio@gmail.com>
2020-12-16 10:09:31 -0500

    dockerize qv-app

--- scripts/deploy/api_deploy_dev.sh
+++ scripts/deploy/api_deploy_dev.sh
@@ -1,3 +1,3 @@
 python /backend/QVtoolapi/manage.py makemigrations
 python /backend/QVtoolapi/manage.py migrate
-python /backend/QVtoolapi/manage.py runserver
+python /backend/QVtoolapi/manage.py runserver 0.0.0.0:8000
