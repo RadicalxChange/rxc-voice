b06ab3c70d74e03ecc960c76b1fd7662c44c81fe
alexrandaccio <acrandaccio@gmail.com>
2020-12-22 19:04:40 -0500

    added description

--- polis-app/src/components/Home/Home.tsx
+++ polis-app/src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Conversation } from "../../models/Conversation";
 import ConvoCard from "./components/ConvoCard";
 import { BgColor } from "../../models/BgColor";
@@ -8,12 +8,12 @@ import "./Home.scss";
 
 function Home(props:any) {
   var activeList: any = [];
-  var archivedList: any = [];
+  var pastList: any = [];
   props.conversations.forEach((conversation: Conversation) => {
     if (moment(conversation.end_date) > moment()) {
       activeList.push(conversation);
     } else {
-      archivedList.push(conversation);
+      pastList.push(conversation);
     }
   });
 
@@ -23,11 +23,63 @@ function Home(props:any) {
    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const [showAbout, setShowAbout] = useState(false);
+
   return (
     <div className="home">
-      <div className="convo-list">
+      <div className="about">
         <h1>RxC Conversations</h1>
-        <p>Click on a conversation below to participate!</p>
+        <span>
+          Welcome to RxC Conversations, RadicalxChange’s platform for
+          deliberation at scale! Click on a topic to share your insights,
+          engage with others, and push important conversations forward
+          with the power of Pol.is!
+        </span>
+        {!showAbout ? (
+          <button
+            type="button"
+            className="show-hide"
+            onClick={() => setShowAbout(showAbout => true)}
+          >
+          How to participate
+          </button>
+        ) : (
+          <div className="about">
+            <h3>Voting</h3>
+            <span>
+              When you click on a conversation, you will see a stack of
+              comments posted by other users. Click to “agree” or “disagree”
+              with each comment--your opinions are crucial to advancing the
+              conversation.
+            </span>
+            <h3>Commenting</h3>
+            <span>
+              We want to hear your unique perspective! We encourage you to post
+              as many of your own comments as you like. Great comments might
+              communicate new insights, opinions, or values/principles you feel
+              are relevant to the conversation. However, make sure that each
+              comment contains only one distinct idea. If it's possible that
+              someone else might agree with part of your comment and disagree
+              with another, then you should split those two parts into separate
+              comments. Comments that contain more than one idea may be taken
+              down or altered by moderators.
+            </span>
+            <a
+              className="polis-link"
+              href="https://roamresearch.com/#/app/polis-methods/page/yYRydgFpz"
+            >Read more in the official Pol.is documentation.
+            </a>
+            <button
+              type="button"
+              className="show-hide"
+              onClick={() => setShowAbout(showAbout => false)}
+            >
+            Show less
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="convo-list">
         <h2>Active</h2>
           {activeList.length ? (
             <ul>
@@ -38,10 +90,10 @@ function Home(props:any) {
           ) : (
             <h3>None</h3>
           )}
-        <h2>Archived</h2>
-          {archivedList.length ? (
+        <h2>Past</h2>
+          {pastList.length ? (
             <ul>
-              {archivedList.map((conversation: Conversation) => (
+              {pastList.map((conversation: Conversation) => (
                 <ConvoCard conversation={conversation} key={conversation.id} />
               ))}
             </ul>
