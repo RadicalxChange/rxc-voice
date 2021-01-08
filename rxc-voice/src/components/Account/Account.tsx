417f7752f3402335ef2f3a49bc6ba1137ce20d6c
alexrandaccio <acrandaccio@gmail.com>
2021-01-08 15:37:16 -0500

    created account page

--- rxc-voice/src/components/Account/Account.tsx
+++ rxc-voice/src/components/Account/Account.tsx
@@ -1,13 +1,14 @@
 import React, { useContext, useEffect } from "react";
-import { ActionContext, StateContext } from "../../hooks";
+import { ActionContext } from "../../hooks";
 import { BgColor } from "../../models/BgColor";
+import defaultPic from '../../assets/icons/profile_icon.svg';
 
 import "./Account.scss";
+import { WebService } from "../../services";
 
 function Account() {
   const { setColor, logoutUser } = useContext(ActionContext);
-  const { user } = useContext(StateContext);
-  console.log("account" + user);
+  const user = WebService.userobj;
 
   useEffect(() => {
     setColor(BgColor.Yellow);
@@ -17,6 +18,23 @@ function Account() {
 
   return (
     <div className="account">
+      <h1 className="title">Account</h1>
+      {user ? (
+        <div className="delegate-card" key={user.id} >
+          {user.profile_pic ? (
+            <img src={user.profile_pic} className="profile-pic" alt="profile-pic" />
+          ) : (
+            <img src={defaultPic} className="profile-pic" alt="profile-pic" />
+          )}
+          <div className="info">
+            <h3 className="name">{user.first_name + " " + user.last_name}</h3>
+            <h3 className="email">{user.email}</h3>
+            <h3 className="credit-balance">Credit Balance: {user.credit_balance}</h3>
+          </div>
+        </div>
+      ) : (
+        <h2>User not logged in.</h2>
+      )}
       <button
         type="button"
         className="logout-button"
