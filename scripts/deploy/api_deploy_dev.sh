789a4b16315038ca53d181d19d1e54cb2fd2973a
alexrandaccio <acrandaccio@gmail.com>
2021-01-11 16:28:02 -0500

    update scripts

--- scripts/deploy/api_deploy_dev.sh
+++ scripts/deploy/api_deploy_dev.sh
@@ -1,5 +1,3 @@
-export $(grep -v '^#' .env | xargs -d '\n')
 python3 ./backend/QVtoolapi/manage.py makemigrations
 python3 ./backend/QVtoolapi/manage.py migrate
-python3 ./backend/QVtoolapi/manage.py shell < ./backend/QVtoolapi/makeadmindelegate.py
 python3 ./backend/QVtoolapi/manage.py runserver 0.0.0.0:8000
