e56ccee2a0c74ed0f8894288ada1f6ee5091b8f4
alexrandaccio <acrandaccio@gmail.com>
2021-02-15 16:46:25 -0500

    user updates hotfix

--- backend/QVtoolapi/main/serializers.py
+++ backend/QVtoolapi/main/serializers.py
@@ -115,13 +115,13 @@ class UserSerializer(serializers.ModelSerializer):
         user.groups.set(validated_data.get('groups', []))
         return user
 
-    def update(self, instance, validated_data):
-        instance.username = validated_data.get('username', instance.username)
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.set_password(validated_data['password'])
-        return instance
+    # def update(self, instance, validated_data):
+    #     instance.username = validated_data.get('username', instance.username)
+    #     instance.first_name = validated_data.get('first_name', instance.first_name)
+    #     instance.last_name = validated_data.get('last_name', instance.last_name)
+    #     instance.email = validated_data.get('email', instance.email)
+    #     instance.set_password(validated_data['password'])
+    #     return instance
 
 
 class DelegateSerializer(serializers.ModelSerializer):
