fc029d9bfd4c7fe2348a8244b0ba6a2961c586fd
alexrandaccio <acrandaccio@gmail.com>
2021-02-16 13:56:35 -0500

    hide unverified delegates

--- backend/QVtoolapi/main/serializers.py
+++ backend/QVtoolapi/main/serializers.py
@@ -212,7 +212,7 @@ class TransferSerializer(serializers.ModelSerializer):
 class PrivateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name']
+        fields = ['first_name', 'last_name', 'is_active']
 
 
 class PrivateDelegateSerializer(serializers.ModelSerializer):
