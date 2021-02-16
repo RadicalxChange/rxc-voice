19e8d464c97b46db41aece8567ddfaf3f36c808f
alexrandaccio <acrandaccio@gmail.com>
2021-02-16 09:32:53 -0500

    change password

--- backend/QVtoolapi/main/serializers.py
+++ backend/QVtoolapi/main/serializers.py
@@ -117,13 +117,14 @@ class UserSerializer(serializers.ModelSerializer):
         user.groups.set(validated_data.get('groups', []))
         return user
 
-    # def update(self, instance, validated_data):
-    #     instance.username = validated_data.get('username', instance.username)
-    #     instance.first_name = validated_data.get('first_name', instance.first_name)
-    #     instance.last_name = validated_data.get('last_name', instance.last_name)
-    #     instance.email = validated_data.get('email', instance.email)
-    #     instance.set_password(validated_data['password'])
-    #     return instance
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
 
 
 class DelegateSerializer(serializers.ModelSerializer):
@@ -153,7 +154,6 @@ class DelegateSerializer(serializers.ModelSerializer):
 
 
 class TransferSerializer(serializers.ModelSerializer):
-    # sender = DelegateSerializer(required=True)
 
     class Meta:
         model = Transfer
