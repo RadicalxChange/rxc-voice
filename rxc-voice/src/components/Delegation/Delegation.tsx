511a90df29ad6b24eae1692636e9a6735b3f0e65
alexrandaccio <acrandaccio@gmail.com>
2021-04-21 12:11:48 -0400

    loading hook

--- rxc-voice/src/components/Delegation/Delegation.tsx
+++ rxc-voice/src/components/Delegation/Delegation.tsx
@@ -17,7 +17,7 @@ import "./Delegation.scss";
 
 function Delegation() {
   const { processId } = useParams<ProcessPageRouteParams>();
-  const { selectedProcess, creditBalance } = useContext(StateContext);
+  const { selectedProcess, creditBalance, loading } = useContext(StateContext);
   const { selectProcess, updateCreditBalance, setColor } = useContext(ActionContext);
   const conversation = getConversation(selectedProcess);
   const [transfers, setTransfers] = useState(new Array<Transfer>());
@@ -97,109 +97,115 @@ function Delegation() {
     return subtotal;
   };
 
-  return (
-    selectedProcess ? (
-      <div className="delg-page">
-        <div className="nav">
-          <ProcessMenu />
-        </div>
-        <TransferModal
-          invite={inviteModal}
-          recipient={stagedTransfer}
-          submitTransfer={submitTransfer}
-          closeModal={closeModal}
-        />
-        <div className="body">
-          <div className="delegation-content">
-            <h1>Delegation</h1>
-            <h2>{getTitle(selectedProcess)}</h2>
-            <div className="explain-text">
-              <p>Welcome to the RxC Voice democratic process! We want to make this decision democratically, so we have to start by deciding who gets to participate. Let’s start with why you’re here. Someone thought you should have a say in this decision, so they gave you some of their voice credits. Voice credits are used for voting in the election later on.</p>
-              <p>Is there anyone you don’t see in the list below that you think should have a say? Send them some of your voice credits to invite them.
-                <strong> Note:</strong> to keep the dry run private, we ask that you please only invite "new" people from{" "}
-                <a className="inline-link" href="https://docs.google.com/spreadsheets/d/1QLTC4aqvICnb3_YuUJvD9UQBQdEQqZev5MRdK8lEHnI/edit?usp=sharing"
-                >this list.
-                </a>
-              </p>
-              <p>You can also give voice credits to someone who is already here if you trust them and want them to have greater influence in the election. At the end of this stage, all voice credit transfers will be matched using Quadratic Funding! If you want to save all of your credits for your own use in the election, that’s fine too.</p>
-              <p>Keep in mind, the threshold for participating in Delegation and Election is 25 voice credits. If you want to participate in the decision, <strong>make sure you keep at least 25 voice credits for yourself.</strong></p>
-            </div>
-            {conversation && delegationOngoing ? (
-              <>
-              <p className="explain-text"><strong>The Delegation Stage closes on {moment(conversation.start_date).format('MMMM Do YYYY, h:mm a')}</strong></p>
-              <h3 className="matching-pool">Matching Fund: {getMatchingPool(selectedProcess)} voice credits.</h3>
-              <button
-                type="button"
-                className="submit-button"
-                onClick={() => setInviteModal(true)}
-              >
-                + Invite someone else
-              </button>
-              </>
-            ) : (
-              <>
-              <p className="explain-text"><strong>The Delegation Stage has concluded. You can see the final delegate list below!</strong></p>
-              <div className="transfers">
-                <h2>Your transfers</h2>
-                <div className="transfers-header">
-                  <h3 className="type">Type</h3>
-                  <h3 className="amount">Amount</h3>
+  if (loading) {
+    return (
+      <h2>Loading...</h2>
+    );
+  } else {
+    return (
+      selectedProcess ? (
+        <div className="delg-page">
+          <div className="nav">
+            <ProcessMenu />
+          </div>
+          <TransferModal
+            invite={inviteModal}
+            recipient={stagedTransfer}
+            submitTransfer={submitTransfer}
+            closeModal={closeModal}
+          />
+          <div className="body">
+            <div className="delegation-content">
+              <h1>Delegation</h1>
+              <h2>{getTitle(selectedProcess)}</h2>
+              <div className="explain-text">
+                <p>Welcome to the RxC Voice democratic process! We want to make this decision democratically, so we have to start by deciding who gets to participate. Let’s start with why you’re here. Someone thought you should have a say in this decision, so they gave you some of their voice credits. Voice credits are used for voting in the election later on.</p>
+                <p>Is there anyone you don’t see in the list below that you think should have a say? Send them some of your voice credits to invite them.
+                  <strong> Note:</strong> to keep the dry run private, we ask that you please only invite "new" people from{" "}
+                  <a className="inline-link" href="https://docs.google.com/spreadsheets/d/1QLTC4aqvICnb3_YuUJvD9UQBQdEQqZev5MRdK8lEHnI/edit?usp=sharing"
+                  >this list.
+                  </a>
+                </p>
+                <p>You can also give voice credits to someone who is already here if you trust them and want them to have greater influence in the election. At the end of this stage, all voice credit transfers will be matched using Quadratic Funding! If you want to save all of your credits for your own use in the election, that’s fine too.</p>
+                <p>Keep in mind, the threshold for participating in Delegation and Election is 25 voice credits. If you want to participate in the decision, <strong>make sure you keep at least 25 voice credits for yourself.</strong></p>
+              </div>
+              {conversation && delegationOngoing ? (
+                <>
+                <p className="explain-text"><strong>The Delegation Stage closes on {moment(conversation.start_date).format('MMMM Do YYYY, h:mm a')}</strong></p>
+                <h3 className="matching-pool">Matching Fund: {getMatchingPool(selectedProcess)} voice credits.</h3>
+                <button
+                  type="button"
+                  className="submit-button"
+                  onClick={() => setInviteModal(true)}
+                >
+                  + Invite someone else
+                </button>
+                </>
+              ) : (
+                <>
+                <p className="explain-text"><strong>The Delegation Stage has concluded. You can see the final delegate list below!</strong></p>
+                <div className="transfers">
+                  <h2>Your transfers</h2>
+                  <div className="transfers-header">
+                    <h3 className="type">Type</h3>
+                    <h3 className="amount">Amount</h3>
+                  </div>
+                  {transfers.length ? (
+                    <>
+                      <ul className="transfer-list">
+                        {transfers.map((transfer: Transfer) => (
+                          <TransferCard transfer={transfer} key={transfer.id}></TransferCard>
+                        ))}
+                      </ul>
+                      <div className="transfers-subtotals">
+                        <div className="type">
+                          <h3>Subtotal</h3>
+                          <h3>Matching Funds Received</h3>
+                        </div>
+                        <div className="amount">
+                          <h3>{(subtotal < 0) ? "- " : "+ "}{Math.abs(subtotal)} voice credits</h3>
+                          <h3>{(match < 0) ? "- " : "+ "}{Math.abs(match)} voice credits</h3>
+                        </div>
+                      </div>
+                      <div className="transfers-subtotals">
+                        <h3 className="total">Total Change</h3>
+                        <h3 className="total">{(subtotal + match < 0) ? "- " : "+ "}{Math.abs(subtotal + match)} voice credits</h3>
+                      </div>
+                    </>
+                  ) : (
+                    <h3>You did not send or receive any transfers.</h3>
+                  )}
                 </div>
-                {transfers.length ? (
-                  <>
-                    <ul className="transfer-list">
-                      {transfers.map((transfer: Transfer) => (
-                        <TransferCard transfer={transfer} key={transfer.id}></TransferCard>
+                </>
+              )}
+              {getDelegates(selectedProcess)!.filter(isVerified).length ? (
+                <>
+                  <h2>Delegates</h2>
+                  <ul className="delegate-list">
+                    {getDelegates(selectedProcess)!
+                      .filter(isVerified)
+                      .map((delegate: Delegate) => (
+                        <DelegateCard
+                          key={delegate.id}
+                          delegate={delegate}
+                          process={selectedProcess}
+                          stageTransfer={setStagedTransfer}
+                        >
+                        </DelegateCard>
                       ))}
                     </ul>
-                    <div className="transfers-subtotals">
-                      <div className="type">
-                        <h3>Subtotal</h3>
-                        <h3>Matching Funds Received</h3>
-                      </div>
-                      <div className="amount">
-                        <h3>{(subtotal < 0) ? "- " : "+ "}{Math.abs(subtotal)} voice credits</h3>
-                        <h3>{(match < 0) ? "- " : "+ "}{Math.abs(match)} voice credits</h3>
-                      </div>
-                    </div>
-                    <div className="transfers-subtotals">
-                      <h3 className="total">Total Change</h3>
-                      <h3 className="total">{(subtotal + match < 0) ? "- " : "+ "}{Math.abs(subtotal + match)} voice credits</h3>
-                    </div>
                   </>
-                ) : (
-                  <h3>You did not send or receive any transfers.</h3>
-                )}
-              </div>
-              </>
-            )}
-            {getDelegates(selectedProcess)!.filter(isVerified).length ? (
-              <>
-                <h2>Delegates</h2>
-                <ul className="delegate-list">
-                  {getDelegates(selectedProcess)!
-                    .filter(isVerified)
-                    .map((delegate: Delegate) => (
-                      <DelegateCard
-                        key={delegate.id}
-                        delegate={delegate}
-                        process={selectedProcess}
-                        stageTransfer={setStagedTransfer}
-                      >
-                      </DelegateCard>
-                    ))}
-                  </ul>
-                </>
-            ) : (
-              <h3>No delegates found.</h3>
-            )}
+              ) : (
+                <h3>No delegates found.</h3>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    ) : (
-      <h1>Process not found. Return to home.</h1>
-    )
-  );
+      ) : (
+        <h1>Process not found. Return to home.</h1>
+      )
+    );
+  }
 }
 
 export default Delegation;
--- rxc-voice/src/components/Deliberation/Deliberation.tsx
+++ rxc-voice/src/components/Deliberation/Deliberation.tsx
@@ -12,9 +12,8 @@ import "./Deliberation.scss";
 function Deliberation() {
   const POLIS_SITE_ID = 'polis_site_id_cG2opQF5hsqj9jGCsr';
   const { processId } = useParams<ProcessPageRouteParams>();
-  const { selectedProcess, creditBalance } = useContext(StateContext);
+  const { selectedProcess, creditBalance, loading } = useContext(StateContext);
   const { selectProcess, setColor } = useContext(ActionContext);
-  const conversation = getConversation(selectedProcess);
 
   useEffect(() => {
     setColor(BgColor.White);
@@ -45,76 +44,82 @@ function Deliberation() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [processId]);
 
-  return (
-    <div className="polis-page">
-      <div className="nav">
-        <ProcessMenu />
-      </div>
-      <div className="body">
-      <h1>Deliberation</h1>
-      <h2 className="content-header">{getTitle(selectedProcess)}</h2>
-      <div className="explain-text">
-        <p>Join us as we collectively draft a ballot of proposals to vote on in our election! Submit proposals, share your thoughts, and show your agreement or disagreement with other delegates’ submissions. This is your chance to influence the ballot of items that voters consider in the final election.</p>
-        <p>Want to know more about who else gets a say in this process? Go back to the Delegation Stage to see how the delegation was determined democratically.</p>
-      </div>
-      {(conversation && creditBalance) ? (
-          (moment() < moment(conversation.end_date)) ? (
-            (moment() > moment(conversation.start_date)) ? (
-              <>
-              <p className="explain-text"><strong>The Deliberation Stage closes on {moment(conversation.end_date).format('MMMM Do YYYY, h:mm a')}</strong></p>
-              {(creditBalance! >= 25) ? (
+  if (loading) {
+    return (
+      <h2>Loading...</h2>
+    );
+  } else {
+    return (
+      <div className="polis-page">
+        <div className="nav">
+          <ProcessMenu />
+        </div>
+        <div className="body">
+        <h1>Deliberation</h1>
+        <h2 className="content-header">{getTitle(selectedProcess)}</h2>
+        <div className="explain-text">
+          <p>Join us as we collectively draft a ballot of proposals to vote on in our election! Submit proposals, share your thoughts, and show your agreement or disagreement with other delegates’ submissions. This is your chance to influence the ballot of items that voters consider in the final election.</p>
+          <p>Want to know more about who else gets a say in this process? Go back to the Delegation Stage to see how the delegation was determined democratically.</p>
+        </div>
+        {(getConversation(selectedProcess) && creditBalance) ? (
+            (moment() < moment(getConversation(selectedProcess)!.end_date)) ? (
+              (moment() > moment(getConversation(selectedProcess)!.start_date)) ? (
                 <>
-                <div
-                  id="polis-iframe"
-                  className='polis'
-                  data-page_id={conversation.uuid}
-                  data-site_id={POLIS_SITE_ID}
-                  data-topic={conversation.title}
-                  data-ucv={moment(conversation.start_date) < moment()}
-                  data-ucw={moment(conversation.start_date) < moment()}
-                  data-ucsd='false'
-                  data-xid={userobj().token}
-                  data-auth_needed_to_vote='false'
-                  data-auth_needed_to_write='false'
-                  data-auth_opt_fb='false'
-                  data-auth_opt_tw='false'
-                >
-                </div>
-                {conversation.show_report ? (
-                  <iframe
-                    title="conversation-results"
-                    className="results-iframe"
-                    src={"https://pol.is/report/" + conversation.report_id}
+                <p className="explain-text"><strong>The Deliberation Stage closes on {moment(getConversation(selectedProcess)!.end_date).format('MMMM Do YYYY, h:mm a')}</strong></p>
+                {(creditBalance! >= 25) ? (
+                  <>
+                  <div
+                    id="polis-iframe"
+                    className='polis'
+                    data-page_id={getConversation(selectedProcess)!.uuid}
+                    data-site_id={POLIS_SITE_ID}
+                    data-topic={getConversation(selectedProcess)!.title}
+                    data-ucv={moment(getConversation(selectedProcess)!.start_date) < moment()}
+                    data-ucw={moment(getConversation(selectedProcess)!.start_date) < moment()}
+                    data-ucsd='false'
+                    data-xid={userobj().token}
+                    data-auth_needed_to_vote='false'
+                    data-auth_needed_to_write='false'
+                    data-auth_opt_fb='false'
+                    data-auth_opt_tw='false'
                   >
-                  </iframe>
-                ) : null}
+                  </div>
+                  {getConversation(selectedProcess)!.show_report ? (
+                    <iframe
+                      title="conversation-results"
+                      className="results-iframe"
+                      src={"https://pol.is/report/" + getConversation(selectedProcess)!.report_id}
+                    >
+                    </iframe>
+                  ) : null}
+                  </>
+                ) : (
+                  <p className="insufficient-credits">Sorry! You do not have enough voice credits to participate in Deliberation or Election. The threshold for participation is 25 voice credits.</p>
+                )}
                 </>
               ) : (
-                <p className="insufficient-credits">Sorry! You do not have enough voice credits to participate in Deliberation or Election. The threshold for participation is 25 voice credits.</p>
-              )}
-              </>
+                <p className="explain-text"><strong>The Deliberation Stage begins on {moment(getConversation(selectedProcess)!.start_date).format('MMMM Do YYYY, h:mm a')}</strong></p>
+              )
             ) : (
-              <p className="explain-text"><strong>The Deliberation Stage begins on {moment(conversation.start_date).format('MMMM Do YYYY, h:mm a')}</strong></p>
+              <div className="body">
+                <p className="explain-text"><strong>The Deliberation Stage has concluded. You can see the results of the conversation below!</strong></p>
+                <iframe
+                  title="conversation-results"
+                  className="results-iframe"
+                  src={"https://pol.is/report/" + getConversation(selectedProcess)!.report_id}
+                >
+                </iframe>
+              </div>
             )
-          ) : (
-            <div className="body">
-              <p className="explain-text"><strong>The Deliberation Stage has concluded. You can see the results of the conversation below!</strong></p>
-              <iframe
-                title="conversation-results"
-                className="results-iframe"
-                src={"https://pol.is/report/" + conversation.report_id}
-              >
-              </iframe>
-            </div>
-          )
-      ) : (
-        <div className="body">
-          <h3>Conversation not found. Head back to home to find what you're looking for.</h3>
+        ) : (
+          <div className="body">
+            <h3>Conversation not found. Head back to home to find what you're looking for.</h3>
+          </div>
+        )}
         </div>
-      )}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Deliberation;
