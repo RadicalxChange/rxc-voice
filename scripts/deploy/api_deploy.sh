98ba4cfcd690f0650323017b98979a6451ba8c76
alexrandaccio <acrandaccio@gmail.com>
2021-04-30 13:03:38 -0400

    makemigrations main

--- scripts/deploy/api_deploy.sh
+++ scripts/deploy/api_deploy.sh
@@ -1,4 +1,4 @@
-python /backend/RxcVoiceApi/manage.py makemigrations
-python /backend/RxcVoiceApi/manage.py migrate
+python /backend/RxcVoiceApi/manage.py makemigrations main
+python /backend/RxcVoiceApi/manage.py migrate main
 python /backend/RxcVoiceApi/manage.py collectstatic --noinput
 gunicorn --workers=3 RxcVoiceApi.wsgi:application --bind 0.0.0.0:8000
--- scripts/deploy/api_deploy_dev.sh
+++ scripts/deploy/api_deploy_dev.sh
@@ -1,3 +1,3 @@
-python3 /backend/RxcVoiceApi/manage.py makemigrations
-python3 /backend/RxcVoiceApi/manage.py migrate
+python3 /backend/RxcVoiceApi/manage.py makemigrations main
+python3 /backend/RxcVoiceApi/manage.py migrate main
 python3 /backend/RxcVoiceApi/manage.py runserver 0.0.0.0:8000
