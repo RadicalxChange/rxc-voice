beff577a830c48d8212d14bface3097285877045
alexrandaccio <acrandaccio@gmail.com>
2021-07-29 15:48:45 -0400

    edit account details

--- backend/RxcVoiceApi/main/serializers.py
+++ backend/RxcVoiceApi/main/serializers.py
@@ -167,7 +167,8 @@ class UserSerializer(serializers.ModelSerializer):
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.email = validated_data.get('email', instance.email)
-        instance.set_password(validated_data['password'])
+        if validated_data.get('password', '') != '':
+            instance.set_password(validated_data['password'])
         instance.save()
         return instance
 
