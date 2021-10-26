53bb636c4135ae536be2544906da5bdef9cebf39
Alex Randaccio <48193894+alexrandaccio@users.noreply.github.com>
2021-10-26 13:53:10 -0400

    404 not found page

--- rxc-voice/src/App.tsx
+++ rxc-voice/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Route, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import { StateContext } from './hooks';
 import Home from "./components/Home";
 import Header from './components/Header';
@@ -12,7 +12,8 @@ import Delegation from './components/Delegation';
 import Deliberation from './components/Deliberation';
 import Election from './components/Election';
 import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword/ResetPassword';
+import ResetPassword from './components/ResetPassword';
+import NotFound from './components/NotFound';
 import { userIsVerified } from './utils';
 
 import './App.scss';
@@ -45,74 +46,82 @@ function App() {
   } else if (!userIsVerified(user)) {
     return (
       <div className="App" style={{ background: color }} >
-        <Route
-          path="/"
-          exact
-          render={() => <Login />}
-        />
-        <Route
-          path="/forgot-password"
-          exact
-          render={() => <ForgotPassword />}
-        />
-        <Route
-          path="/oauth2/callback"
-          render={() => <Callback></Callback>}
-        />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => <Login />}
+          />
+          <Route
+            path="/forgot-password"
+            exact
+            render={() => <ForgotPassword />}
+          />
+          <Route
+            path="/oauth2/callback"
+            render={() => <Callback />}
+          />
+          <Route render={() => <NotFound />} />
+        </Switch>
       </div>
     );
   }
   return (
     <div className="App" style={{ background: color }} >
 
-      <Header></Header>
+      <Switch>
+        <Header></Header>
 
-      <Route
-        path="/"
-        exact
-        render={() => <Home></Home>}
-      />
+        <Route
+          path="/"
+          exact
+          render={() => <Home></Home>}
+        />
 
-      <Route
-        path="/:processId/:processSlug/Delegation"
-        exact
-        render={() => <Delegation></Delegation>}
-      />
+        <Route
+          path="/:processId/:processSlug/Delegation"
+          exact
+          render={() => <Delegation></Delegation>}
+        />
 
-      <Route
-        path="/:processId/:processSlug/Deliberation"
-        exact
-        render={() => <Deliberation></Deliberation>}
-      />
+        <Route
+          path="/:processId/:processSlug/Deliberation"
+          exact
+          render={() => <Deliberation></Deliberation>}
+        />
 
-      <Route
-        path="/:processId/:processSlug/Election"
-        exact
-        render={() => <Election></Election>}
-      />
+        <Route
+          path="/:processId/:processSlug/Election"
+          exact
+          render={() => <Election></Election>}
+        />
 
-      <Route
-        path="/account"
-        exact
-        render={() => <Account></Account>}
-      />
+        <Route
+          path="/account"
+          exact
+          render={() => <Account></Account>}
+        />
 
-      <Route
-        path="/about"
-        exact
-        render={() => <About></About>}
-      />
+        <Route
+          path="/about"
+          exact
+          render={() => <About></About>}
+        />
 
-      <Route
-        path="/verify"
-        exact
-        render={() => <ValidationPage></ValidationPage>}
-      />
+        <Route
+          path="/verify"
+          exact
+          render={() => <ValidationPage></ValidationPage>}
+        />
 
-      <Route
-        path="/oauth2/callback"
-        render={() => <Callback></Callback>}
-      />
+        <Route
+          path="/oauth2/callback"
+          render={() => <Callback></Callback>}
+        />
+
+
+        <Route render={() => <NotFound />} />
+      </Switch>
 
     </div>
   );
