783e3f61abf9768ab79b778544bc814bbbc5f0e9
Alex Randaccio <48193894+alexrandaccio@users.noreply.github.com>
2022-02-18 15:27:57 -0500

    update About section

--- rxc-voice/src/components/About/About.tsx
+++ rxc-voice/src/components/About/About.tsx
@@ -1,6 +1,10 @@
 import React, { useContext, useEffect } from "react";
 import { ActionContext } from "../../hooks";
 import { BgColor } from "../../models/BgColor";
+import collab_img from "../../assets/collab.png";
+import group_img from "../../assets/group.png";
+import convo_img from "../../assets/speech-bubbles.png";
+import handshake_img from "../../assets/handshake.png";
 
 import "./About.scss";
 
@@ -15,66 +19,124 @@ function About() {
 
   return (
     <div className="about">
-      <div className="header">
-        <h1 className="title">About RxC Voice</h1>
-        <p>RadicalxChange's platform for collective decision making!</p>
-      </div>
-      <h2>Delegation</h2>
-      <span className="explain-text">
-        The process begins with the Delegation Stage. In this stage,
-        you can invite others to participate by sending them
-        voice credits. You can also send voice credits to other existing
-        delegates. At the end of the Delegation Stage, we match all your
-        transfers using{" "}
-        <a
-          href="https://www.radicalxchange.org/concepts/quadratic-funding/"
-          target="_blank"
-          rel="noopener nofollow noreferrer"
-          className="inline-link"
-        >Quadratic Funding</a>! This gives you the power to
-        democratically determine who gets a voice in the decision making
-        process, and how strong each delegate's voice will be.
-      </span>
-      <h3>Matching fund size</h3>
-      <span className="explain-text">
-        The size of the matching pool is determined by the number of voice
-        credits in circulation. We multiply the number of initial delegates by
-        the number of voice credits distributed to each delegate, then round
-        the result to the nearest hundred.
-      </span>
-      <h2>Deliberation</h2>
-      <span className="explain-text">
-        In the Deliberation Stage, you have the power to submit proposals
-        for the final ballot! This stage is a conversation, where delegates
-        can communicate the insights, opinions, or values/principles that
-        they feel are relevant to the decision on the table. You can also
-        "agree" or "disagree" with other delegates' posts. At the end of
-        this stage, the administrators will curate the posts that generated
-        the highest degree of consensus into a user-generated ballot for
-        the final vote.
-      </span>
-      <h2>Election</h2>
-      <span className="explain-text">
-        The Election stage is where delegates will use{" "}
-        <a
-          href="https://www.radicalxchange.org/concepts/quadratic-voting/"
-          target="_blank"
-          rel="noopener nofollow noreferrer"
-          className="inline-link"
-        >Quadratic Voting</a> to vote on the proposals
-        generated in the Deliberation Stage. An essential option on the ballot
-        will be "Ballot Ratification." With this particular ballot item, you
-        can express whether or not you feel that the ballot fairly and
-        accurately represents the delegates’ contributions in the Deliberation
-        Stage. At the end of the Election, if the sum of effective votes for
-        the Ballot Ratification option is less than zero, the results of the
-        Election are nullified and a new ballot must be curated and voted on--
-        so make sure you place a couple of voice credits toward supporting or
-        opposing the Ballot Ratification option, depending on your satisfaction
-        with the ballot curation. If the ballot gets approved, this stage
-        captures a detailed image of individual preferences and publishes them
-        for all delegates to see.
-      </span>
+    <div id="about-info">
+      <section className="panel">
+        <div className="subpanel_image">
+          <img src={collab_img} alt="collaboration" />
+        </div>
+        <div id="first-panel" className="subpanel_text">
+          <h2>Introducing RxC Voice.</h2>
+          <p>
+            <strong>RxC Voice</strong> is a modular way for groups to evolve and make decisions democratically.
+          </p>
+          <ul className="bullets">
+            <li>Form a group.</li>
+            <li>Explore a shared problem.</li>
+            <li>Identify actionable compromises.</li>
+          </ul>
+        </div>
+      </section>
+      <section className="panel">
+        <div className="subpanel_image">
+          <img src={group_img} alt="form a group" />
+        </div>
+        <div className="subpanel_text">
+          <h2>Form a group.</h2>
+          <p>
+            Voice helps groups <strong>surface leadership</strong> by encouraging participants to signal who they trust.
+          </p>
+          <ul className="bullets">
+            <li><strong>Everyone’s voice is equal</strong>: participants start with an equal number of voice credits.</li>
+            <li>Optionally, participants may be permitted to delegate some of their voice credits to others, so <strong>trust and specialized knowledge is accounted for.</strong></li>
+            <li>Widely trusted individuals can receive a voice credit bonus via <a
+              href="http://wtfisqf.com"
+              target="_blank"
+              rel="noopener nofollow noreferrer"
+              className="inline-link"
+            >Quadratic Funding</a>.</li>
+            <li><strong>The group grows organically</strong> when participants transfer voice credits to new contacts.</li>
+            <li>Even after delegating, <strong>everyone can participate directly</strong>.</li>
+          </ul>
+        </div>
+      </section>
+      <section className="panel">
+        <div className="subpanel_image">
+          <img src={convo_img} alt="conversation" />
+        </div>
+        <div className="subpanel_text">
+          <h2>Explore a shared problem.</h2>
+          <p>
+            Voice makes it easy to <strong>explore a shared problem</strong>, identifying and building on areas of consensus.
+          </p>
+          <ul className="bullets">
+            <li><strong>Everyone’s voice is heard</strong> in a Pol.is conversation.</li>
+            <li>Users can share thoughts, feelings, opinions, values, facts, or principles they feel are relevant.</li>
+            <li>There are no replies or threads, so <strong>disagreements don’t derail the agenda</strong>.</li>
+            <li>Users iterate on comments they don’t quite agree with, and thereby <strong>ideas evolve toward consensus</strong>.</li>
+            <li>Pol.is outputs meaningful data about which comments built consensus and which were divisive.</li>
+          </ul>
+        </div>
+      </section>
+      <section className="panel">
+        <div className="subpanel_image">
+          <img src={handshake_img} alt="handshake" />
+        </div>
+        <div className="subpanel_text">
+          <h2>Identify actionable compromises.</h2>
+          <p>
+            Voice helps groups <strong>define their options</strong> and <strong>find compromises</strong> that lead to collective action.
+          </p>
+          <ul className="bullets">
+            <li><strong>Ballot construction is decentralized</strong>. A ballot of actionable options for the group is constructed from user submissions in the Pol.is conversation.</li>
+            <li>An appointed curator ensures that the proposals are coherent. Users can reject the ballot if they disapprove of the curation.</li>
+            <li>A shared decision is reached using <a
+              href="https://quadraticvote.radicalxchange.org/"
+              target="_blank"
+              rel="noopener nofollow noreferrer"
+              className="inline-link"
+            >Quadratic Voting</a> (QV).</li>
+            <li>With QV, users can express the magnitude of their preferences, which means <strong>apathetic majorities don’t swing decisions</strong>.</li>
+            <li>By imposing an increasing cost on strong preferences, <strong>QV incentivizes coalition building and compromise</strong>.</li>
+          </ul>
+        </div>
+      </section>
+      <section id="last-panel">
+        <div>
+          <h2>Group decision making is a public good.</h2>
+          <p>
+            RxC Voice is an open-source <a
+              href="https://github.com/RadicalxChange/rxc-voice"
+              target="_blank"
+              rel="noopener nofollow noreferrer"
+              className="inline-link"
+            >project</a> maintained by the <a
+              href="https://radicalxchange.org/"
+              target="_blank"
+              rel="noopener nofollow noreferrer"
+              className="inline-link"
+            >RadicalxChange Foundation</a>, a 501(c)(3) nonprofit organization.
+          </p>
+          <p>
+            <a
+              href="https://pol.is/"
+              target="_blank"
+              rel="noopener nofollow noreferrer"
+              className="inline-link"
+            >Pol.is</a> is an open-source <a
+              href="https://github.com/pol-is/"
+              target="_blank"
+              rel="noopener nofollow noreferrer"
+              className="inline-link"
+            >project</a> maintained by <a
+              href="https://compdemocracy.org/"
+              target="_blank"
+              rel="noopener nofollow noreferrer"
+              className="inline-link"
+            >The Computational Democracy Project</a>, a 501(c)(3) nonprofit organization.
+          </p>
+        </div>
+      </section>
+    </div>
       <h2 className="privacy-title">Privacy Policy</h2>
       <h3>Definitions</h3>
       <ul className="bullets">
